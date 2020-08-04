9c5e1e8d67ca487b3efd9cbf673f9aa8bd7b5529
unknown <islom.din.nur@gmail.com>
2020-08-04 11:28:56 +0500

    fixed interface PostsAPI

--- Posts/app/src/main/java/din/islom/posts/models/Post.java
+++ Posts/app/src/main/java/din/islom/posts/models/Post.java
@@ -1,4 +1,19 @@
 package din.islom.posts.models;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Post {
+    @SerializedName("userId")
+    private int userID;
+
+    @SerializedName("id")
+    private int id;
+
+    @SerializedName("title")
+    private String title;
+
+    @SerializedName("body")
+    private String body;
+
+
 }
