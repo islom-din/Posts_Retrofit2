df935a529432f2fc60a32b53b312b6cf8a54103b
unknown <islom.din.nur@gmail.com>
2020-08-04 16:15:19 +0500

    Initial retrofit and add permission for internet in Manifest

--- Posts/app/src/main/java/din/islom/posts/MainActivity.java
+++ Posts/app/src/main/java/din/islom/posts/MainActivity.java
@@ -3,12 +3,59 @@ package din.islom.posts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import din.islom.posts.models.Post;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+    private Retrofit retrofit;
+    private PostsAPI postsAPI;
+    private List listOfPosts;
+    private static final String TAG = "MainActivity";
+
+    private final String BASE_URL = "https://jsonplaceholder.typicode.com/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initListOfPosts();
+    }
+
+    private void initListOfPosts() {
+         retrofit = new Retrofit.Builder()
+                 .baseUrl(BASE_URL)
+                 .addConverterFactory(GsonConverterFactory.create())
+                 .build();
+
+         // Создаем объект, при помощи которого будем выполнять запросы
+         postsAPI = retrofit.create(PostsAPI.class);
+
+         postsAPI.getData().enqueue(new Callback<List<Post>>() {
+             @Override
+             public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                 if(!response.isSuccessful()) {
+                     Log.d(TAG, "onResponse: " + response.message());
+                     return;
+                 }
+
+                 listOfPosts = response.body();
+
+             }
+
+             @Override
+             public void onFailure(Call<List<Post>> call, Throwable t) {
+                 Log.d(TAG, "onFailure: " + t.getMessage());
+             }
+         });
     }
 }
