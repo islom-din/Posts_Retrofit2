7378ebd4c1e335cc28fd54e20c7a58e5db5c2b10
unknown <islom.din.nur@gmail.com>
2020-08-05 13:47:58 +0500

    add query parameter

--- Posts/app/src/main/java/din/islom/posts/PostsAPI.java
+++ Posts/app/src/main/java/din/islom/posts/PostsAPI.java
@@ -7,11 +7,14 @@ import din.islom.posts.models.Post;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface PostsAPI {
+//    @GET("posts")
+//    Call<List<Post>> getData();
     @GET("posts")
-    Call<List<Post>> getData();
+    Call<List<Post>> getData(@Query("userId") int userId);
 
-    @GET("posts/{id}/{comments}")
-    Call<List<Comment>> getComments(@Path("id") int postId, @Path("comments") String commentsString);
+    @GET("posts/{id}/comments")
+    Call<List<Comment>> getComments(@Path("id") int postId);
 }
